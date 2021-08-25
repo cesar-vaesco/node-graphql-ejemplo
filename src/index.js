8a39e612ce5cf7ad31e5ad23d6cddfd9ac2af0d2
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-08-25 12:12:23 -0500

    Montando una configuración sencilla de graphql en nuestra app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
 
 
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'Hello World'
+    })
+});
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}));
 
 
 app.listen(3000, () => {
